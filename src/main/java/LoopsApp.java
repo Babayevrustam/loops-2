4b28ee764bd75a18764e3d8a4fbac89d159fbfe5
Babayevrustam <Babayevrustam@github.com>
2023-03-10 01:56:35 +0400

    pools

---
+++ src/main/java/LoopsApp.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class LoopsApp {
+    public static void main(String[] args) {
+        int a = new Scanner(System.in).nextInt();
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println("=====");
+
+        if (a % 2 == 0) {
+            System.out.println("EVEN");
+        } else {
+            System.out.println("ODD");
+        }
+        for (int i = 0; i <= 3; i++) {
+            System.out.println(a);
+        }
+    }
+
+}
