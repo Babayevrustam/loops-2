da33e008ae65d1614bbec5072e597eb6d2c0e685
Babayevrustam <Babayevrustam@github.com>
2023-03-15 21:44:30 +0400

    Divisibility22App

---
+++ src/main/java/az/eolymp/Numberdivisible.java
@@ -0,0 +1,17 @@
+package az.eolymp;
+
+import java.util.Scanner;
+
+public class Numberdivisible {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int n = scan.nextInt();
+        if (n % a == 0 && n % b == 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
