401d60a427b09cb3a70fde43452820f93856da8f
Babayevrustam <Babayevrustam@github.com>
2023-03-15 22:54:52 +0400

    Samenumbpp

---
+++ src/main/java/az/eolymp/SamenumberApp.java
@@ -0,0 +1,15 @@
+package az.eolymp;
+
+import java.util.Scanner;
+
+public class SamenumberApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+            if (n > 0) {
+                System.out.println("1");
+            } else if ((n < 0) && (n == 0)) {
+            }
+                System.out.println("0");
+            }
+    }
