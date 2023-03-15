8ca05c9d56cc5a26fccccd5349a60b58a0bb5671
Babayevrustam <Babayevrustam@github.com>
2023-03-15 21:55:30 +0400

    NumberdiApp

--- src/main/java/az/eolymp/Numberdivisible.java
+++ src/main/java/az/eolymp/Numberdivisible.java
@@ -5,13 +5,12 @@ import java.util.Scanner;
 public class Numberdivisible {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        int n = scan.nextInt();
-        if (n % a == 0 && n % b == 0) {
-            System.out.println("YES");
+        int t = scan.nextInt();
+
+        if (t > 0) {
+            System.out.println("Water");
         } else {
-            System.out.println("NO");
+            System.out.println("Ice");
         }
     }
 }
