92b1bae3b56d16612cc9a8982fd0aa40d76602ef
Babayevrustam <Babayevrustam@github.com>
2023-03-25 22:24:49 +0400

    SumtwonumberAppApp

---
+++ src/main/java/az/eolymp2/SumtwonumberApp.java
@@ -0,0 +1,15 @@
+package az.eolymp2;
+
+import java.util.Scanner;
+
+public class SumtwonumberApp {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            for (int i = 0; i < t; i++) {
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                System.out.println(a + b);
+            }
+        }
+    }
