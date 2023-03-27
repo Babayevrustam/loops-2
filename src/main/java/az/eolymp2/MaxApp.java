53feda8caa6854dca13ae6bad3549f1447e272fa
Babayevrustam <Babayevrustam@github.com>
2023-03-27 15:28:29 +0400

    MaxApp

--- src/main/java/az/eolymp2/MaxApp.java
+++ src/main/java/az/eolymp2/MaxApp.java
@@ -1,7 +1,16 @@
 package az.eolymp2;
 
+import java.util.Scanner;
+
 public class MaxApp {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+        long d = sc.nextLong();
 
+        long max = Math.max( Math.max(a, b), Math.max(c, d));
+        System.out.println(max);
     }
 }
