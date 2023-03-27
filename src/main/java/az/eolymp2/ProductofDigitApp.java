c6ce4c41c26e7212da1f11aa6e56ea8ca26daf7a
Babayevrustam <Babayevrustam@github.com>
2023-03-27 17:05:43 +0400

    ProductofDigitApp

---
+++ src/main/java/az/eolymp2/ProductofDigitApp.java
@@ -0,0 +1,16 @@
+package az.eolymp2;
+
+import java.util.Scanner;
+
+public class ProductofDigitApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int product = 1;
+        while (n > 0) {
+            product *= n % 10;
+            n /= 10;
+        }
+        System.out.println(product);
+    }
+}
